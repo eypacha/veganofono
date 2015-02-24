fdeba1155a784f21822cd4c37bedc978530c8eab
eypacha <pachaguionbajo@gmail.com>
2015-02-24 17:19:56 -0300

    Octava funcional y CapsLock off

--- veganofono.sh
+++ veganofono.sh
@@ -3,22 +3,43 @@
 # por EyPacha! parte del proyecto Musica con Cualquier Cosa
 # www.eypacha.com.ar
 
+# Define constantes
+gr1='\033[1;32m'
+gr2='\033[0;32m'
+red='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Inhabilita el CapsLock
+xkbset nullify lock
+
+# user interrupt trap demo
+function int_handler {
+  clear  
+  echo -e "${gr1}Gracias por usar el Veganófono. ¡Vuelva pronto!${NC}"
+  xkbset nullify -lock
+ exit
+}
+
+# Establish interrupt handler
+trap int_handler INT
+
+# Inicia el bucle a la espera de teclas
 while :
 do
 clear
+echo -e "${gr2}*** ${gr1}Veganófono 0.1 ${gr2}***${NC}"
 read -n 1 -p "Esperando tecla > " mi_tecla
-echo
+echo #$mi_tecla
 
 case $mi_tecla in
-  '') aplay -N -q 01.wav &;;
-  s) aplay -N -q 02.wav &;;
-  d) aplay -N -q 03.wav &;;
-  f) aplay -N -q 04.wav &;;
-  'ga') aplay -N -q 05.wav &;;
-  h) aplay -N -q 06.wav &;;
-  j) aplay -N -q 07.wav &;;
-  k) aplay -N -q 08.wav &;;
+  'D') aplay -N -q 01.wav &;;
+  'A') aplay -N -q 02.wav &;;
+  'C') aplay -N -q 03.wav &;;
+  'B') aplay -N -q 04.wav &;;
+  '') aplay -N -q 05.wav &;;
+  a) aplay -N -q 06.wav &;;
+  w) aplay -N -q 07.wav &;;
+  d) aplay -N -q 08.wav &;;
 esac
 
 done
-
